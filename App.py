92b36f4df9a4284cc0d505a1f5d810a482581e3f
HSMonsterKing <stanleyoreo@yahoo.com.tw>
2026-03-17 15:33:27 +0800

    查詢結果

--- App.py
+++ App.py
@@ -31,3 +31,9 @@ print("cursor 取得成功")
 
 # 3. 透過 cursor 將 sql 送給 azure sql server 執行
 cursor.execute("select * from stocks")
+ # 4. 取得查詢結果存入 records list[]中
+records =cursor.fetchall()
+
+rec_count = len(records)
+
+print(f'總共查詢到{rec_count} 筆資料')
