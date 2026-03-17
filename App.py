ed3d495b8d24a5941b34e7766c9893388793f920
HSMonsterKing <stanleyoreo@yahoo.com.tw>
2026-03-17 14:21:46 +0800

    資料表建立

--- App.py
+++ App.py
@@ -7,6 +7,19 @@ database = "free-sql-db-9475644"
 user = "dbeng"
 password = "Zxcv1234"
 '''密碼洩漏問題'''
-print("import 成功")
+"""
+資料表建立 DDL:
+
+
+CREATE TABLE [dbo].[stocks] (
+    [Id]    INT             IDENTITY (1, 1) NOT NULL,
+    [sid]   CHAR (4)        NOT NULL,
+    [sname] VARBINARY (30)  NOT NULL,
+    [price] DECIMAL (16, 2) NOT NULL,
+    [pdate] DATETIME        CONSTRAINT [DEFAULT_stocks_pdate] DEFAULT GETDATE() NOT NULL,
+    CONSTRAINT [PK_stocks] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
+"""
 
 connect = pymssql.connect(server,user,password,database)
